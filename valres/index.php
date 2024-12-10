b9b847f10b03f803d57df04e0328c8b8fde13ea6
ThomasFLG <159135867+ThomasFLG@users.noreply.github.com>
2024-12-10 20:18:11 +0100

    Delete valres directory

--- valres/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Location: page/accueil.php");
-exit;
-?>
--- valres/page/listeReservation.php
+++
@@ -1,7 +0,0 @@
-<head>
-  <title>Liste Réservation</title>
-</head>
-
-<?php include('../includes/header.php');?>
-
-<?php include('../includes/footer.php'); ?>
